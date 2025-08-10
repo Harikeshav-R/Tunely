001f62cf7fa7d135ddda28f122b03dd18022bc8e
Harikeshav R <r.harikeshav@gmail.com>
2025-08-10 19:56:09 -0400

    refactor(constants): encapsulate constants into a class for better organization
    - Encapsulated application constants into a `Constants` class for improved structure and readability.
    - Updated references for log settings, config, and database defaults to align with the new `Constants` class structure.

--- tunely/utils/constants.py
+++ tunely/utils/constants.py
@@ -2,19 +2,21 @@ from pathlib import Path
 
 from appdirs import user_config_dir, user_cache_dir, user_data_dir
 
-APP_NAME = "Tunely"
-APP_AUTHOR = "Harikeshav R"
 
-DEFAULT_LOG_DIR = Path(user_cache_dir(APP_NAME, APP_AUTHOR), "logs").resolve()
-DEFAULT_LOG_FILE = "tunely.log"
-DEFAULT_MAX_BYTES = 10_000_000
-DEFAULT_BACKUP_COUNT = 5
-DEFAULT_LEVEL = "INFO"
-DEFAULT_LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-DEFAULT_LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+class Constants:
+    APP_NAME = "Tunely"
+    APP_AUTHOR = "Harikeshav R"
 
-DEFAULT_CONFIG_DIR = Path(user_config_dir(APP_NAME, APP_AUTHOR)).resolve()
-DEFAULT_CONFIG_FILE = 'config.ini'
+    DEFAULT_LOG_DIR = Path(user_cache_dir(APP_NAME, APP_AUTHOR), "logs").resolve()
+    DEFAULT_LOG_FILE = "tunely.log"
+    DEFAULT_LOG_MAX_BYTES = 10_000_000
+    DEFAULT_LOG_BACKUP_COUNT = 5
+    DEFAULT_LOG_LEVEL = "INFO"
+    DEFAULT_LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    DEFAULT_LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
-DEFAULT_DATABASE_DIR = Path(user_data_dir(APP_NAME, APP_AUTHOR), 'databases').resolve()
-DEFAULT_DATABASE_FILE = 'tunely.db'
+    DEFAULT_CONFIG_DIR = Path(user_config_dir(APP_NAME, APP_AUTHOR)).resolve()
+    DEFAULT_CONFIG_FILE = 'config.ini'
+
+    DEFAULT_DATABASE_DIR = Path(user_data_dir(APP_NAME, APP_AUTHOR), 'databases').resolve()
+    DEFAULT_DATABASE_FILE = 'tunely.db'
