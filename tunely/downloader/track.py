dc5a4fd6bc6a667228b80b4961a3ccfacc2cd9a9
Harikeshav R <r.harikeshav@gmail.com>
2025-08-12 22:30:12 -0400

    **feat(track): add track download and conversion methods**
    - Added `Track.download_track` method to handle downloading and processing of individual Spotify tracks.
    - Implemented `Track.convert_raw_to_audio` to automate raw audio file conversion using `ffmpeg`.
    - Simplified metadata extraction by refactoring `get_song_info` to replace constants with direct string literals.
    - Enhanced error handling and logging throughout track processing for improved transparency.
    - Integrated lyrics downloading and audio tagging functionality into the track download pipeline.

--- tunely/downloader/track.py
+++ tunely/downloader/track.py
@@ -1,10 +1,18 @@
 import logging
 import math
+import shutil
+
+import ffmpeg
 
 from pathlib import Path
 from typing import Any
 
+from librespot.metadata import TrackId
+
+from tunely.utils.config import Config
 from tunely.utils.constants import Constants
+from tunely.utils.db import Database
+from tunely.utils.helper import Helper
 from tunely.downloader.downloader import Downloader
 
 _logger = logging.getLogger(__name__)
@@ -36,9 +44,9 @@ class Track:
 
             offset += limit
 
-            songs.extend(resp[Constants.SPOTIFY_ITEMS])
+            songs.extend(resp["items"])
 
-            if len(resp[Constants.SPOTIFY_ITEMS]) < limit:
+            if len(resp["items"]) < limit:
                 break
 
         return songs
@@ -58,8 +66,8 @@ class Track:
 
         resp = Downloader.invoke_url(Constants.SPOTIFY_FOLLOWED_ARTISTS_URL)[1]
 
-        for artist in resp[Constants.SPOTIFY_ARTISTS][Constants.SPOTIFY_ITEMS]:
-            artists.append(artist[Constants.SPOTIFY_ID])
+        for artist in resp["artists"]["items"]:
+            artists.append(artist["id"])
 
         return artists
 
@@ -92,40 +100,40 @@ class Track:
         """
 
         _logger.info(f"Getting song info for {song_id}")
-        (raw, info) = Downloader.invoke_url(f'{Constants.SPOTIFY_TRACKS_URL}?ids={song_id}&market=from_token')
+        raw, info = Downloader.invoke_url(f'{Constants.SPOTIFY_TRACKS_URL}?ids={song_id}&market=from_token')
 
-        if not Constants.SPOTIFY_TRACKS in info:
+        if not "tracks" in info:
             _logger.error(f'Invalid response from TRACKS_URL:\n{raw}')
             raise ValueError(f'Invalid response from TRACKS_URL:\n{raw}')
 
         try:
             artists = []
-            for data in info[Constants.SPOTIFY_TRACKS][0][Constants.SPOTIFY_ARTISTS]:
-                artists.append(data[Constants.SPOTIFY_NAME])
+            for data in info["tracks"][0]["artists"]:
+                artists.append(data["name"])
 
-            album_name = info[Constants.SPOTIFY_TRACKS][0][Constants.SPOTIFY_ALBUM][Constants.SPOTIFY_NAME]
-            name = info[Constants.SPOTIFY_TRACKS][0][Constants.SPOTIFY_NAME]
+            album_name = info["tracks"][0]["album"]["name"]
+            name = info["tracks"][0]["name"]
             release_year = \
-                info[Constants.SPOTIFY_TRACKS][0][Constants.SPOTIFY_ALBUM][Constants.SPOTIFY_RELEASE_DATE].split('-')[0]
-            disc_number = info[Constants.SPOTIFY_TRACKS][0][Constants.SPOTIFY_DISC_NUMBER]
-            track_number = info[Constants.SPOTIFY_TRACKS][0][Constants.SPOTIFY_TRACK_NUMBER]
-            scraped_song_id = info[Constants.SPOTIFY_TRACKS][0][Constants.SPOTIFY_ID]
-            is_playable = info[Constants.SPOTIFY_TRACKS][0][Constants.SPOTIFY_IS_PLAYABLE]
-            duration_ms = info[Constants.SPOTIFY_TRACKS][0][Constants.SPOTIFY_DURATION_MS]
+                info["tracks"][0]["album"]["release_date"].split('-')[0]
+            disc_number = info["tracks"][0]["disc_number"]
+            track_number = info["tracks"][0]["track_number"]
+            scraped_song_id = info["tracks"][0]["id"]
+            is_playable = info["tracks"][0]["is_playable"]
+            duration_ms = info["tracks"][0]["duration_ms"]
 
-            image = info[Constants.SPOTIFY_TRACKS][0][Constants.SPOTIFY_ALBUM][Constants.SPOTIFY_IMAGES][0]
-            for i in info[Constants.SPOTIFY_TRACKS][0][Constants.SPOTIFY_ALBUM][Constants.SPOTIFY_IMAGES]:
-                if i[Constants.SPOTIFY_WIDTH] > image[Constants.SPOTIFY_WIDTH]:
+            image = info["tracks"][0]["album"]["images"][0]
+            for i in info["tracks"][0]["album"]["images"]:
+                if i["width"] > image["width"]:
                     image = i
-            image_url = image[Constants.SPOTIFY_URL]
+            image_url = image["url"]
 
             _logger.info(f"Song info for {song_id} retrieved")
 
-            return artists, info[Constants.SPOTIFY_TRACKS][0][
-                Constants.SPOTIFY_ARTISTS], album_name, name, image_url, release_year, disc_number, track_number, scraped_song_id, is_playable, duration_ms
+            return artists, info["tracks"][0][
+                "artists"], album_name, name, image_url, release_year, disc_number, track_number, scraped_song_id, is_playable, duration_ms
 
         except Exception as e:
-            raise ValueError(f'Failed to parse TRACKS_URL response: {str(e)}\n{raw}')
+            raise ValueError(f'Failed to parse TRACKS_URL response: {e.with_traceback(NameError)}\n{raw}')
 
     @staticmethod
     def get_song_lyrics(song_id: str, lyrics_file_path: Path) -> None:
@@ -174,3 +182,128 @@ class Track:
 
         _logger.error(f'Failed to fetch lyrics: {song_id}')
         raise ValueError(f'Failed to fetch lyrics: {song_id}')
+
+    @staticmethod
+    def convert_raw_to_audio(source_file_path: Path) -> None:
+        temp_file_path = source_file_path.with_suffix('.temp')
+        shutil.move(source_file_path, temp_file_path)
+
+        download_format = Config.get("downloader", "format", Constants.CONFIG_DOWNLOADER_FORMAT)
+        codec = Constants.SPOTIFY_CODEC_MAP.get(download_format.lower(), "copy")
+
+        if codec != "copy":
+            bitrates = {
+                'auto': '320k' if Downloader.check_premium() else '160k',
+                'normal': '96k',
+                'high': '160k',
+                'very_high': '320k'
+            }
+            bitrate = bitrates[Config.get("downloader", "quality", Constants.CONFIG_DOWNLOADER_QUALITY)]
+
+        else:
+            bitrate = None
+
+        try:
+            stream = ffmpeg.input(temp_file_path)
+
+            ffmpeg.output(
+                stream,
+                source_file_path,
+                **{
+                    'c:a': codec,
+                    **({'b:a': bitrate} if bitrate else {})
+                }
+            )
+
+            temp_file_path.unlink(missing_ok=True)
+            _logger.info(f"Converted raw to audio: {source_file_path}")
+
+        except ffmpeg.Error as e:
+            _logger.error(f"Failed to convert raw to audio: {e.stderr.decode()}")
+
+    @staticmethod
+    def download_track(track_id: str):
+        try:
+            artists, raw_artists, album_name, name, image_url, release_year, disc_number, track_number, scraped_song_id, is_playable, duration_ms = Track.get_song_info(
+                track_id)
+
+            song_name = Helper.fix_file_name(name)
+            extension = Constants.SPOTIFY_EXT_MAP.get(
+                Config.get("downloader", "format", Constants.CONFIG_DOWNLOADER_FORMAT).lower())
+
+            file_path = Path(Config.get("downloader", "output_dir", Constants.CONFIG_DOWNLOADER_OUTPUT_DIR),
+                             Helper.fix_file_name(artists[0]),
+                             Helper.fix_file_name(album_name),
+                             f"{track_number} - {Helper.fix_file_name(song_name)}.{extension}")
+
+        except Exception as e:
+            _logger.error(f"Failed to download track {track_id}: {e.with_traceback(None)}")
+            return
+
+        _logger.info(f"Downloading track {track_id} to {file_path}")
+
+        try:
+            if not is_playable:
+                _logger.error(f"Skipping track {track_id} because it is not playable")
+                return
+
+            if Database.get_downloaded_songs_by_id(track_id) is not None or file_path.exists():
+                _logger.info(f"Skipping track {track_id} because it is already downloaded")
+                return
+
+            if track_id != scraped_song_id:
+                track_id = scraped_song_id
+
+            track = TrackId.from_base62(track_id)
+            stream = Downloader.get_content_stream(track, Config.get("downloader", "quality",
+                                                                     Constants.CONFIG_DOWNLOADER_QUALITY))
+
+            file_path.parent.mkdir(parents=True, exist_ok=True)
+
+            with open(file_path, 'wb') as f:
+                b = 0
+
+                while b < 5:
+                    data = stream.input_stream.stream().read(
+                        Config.get("downloader", "chunk_size", Constants.CONFIG_DOWNLOADER_CHUNK_SIZE))
+                    f.write(data)
+                    b += 1 if data == b'' else 0
+
+            _logger.info(f"Raw data for track {track_id} downloaded")
+
+            download_lyrics = Config.get("downloader", "download_lyrics", Constants.CONFIG_DOWNLOADER_DOWNLOAD_LYRICS)
+            download_lyrics_flag = True
+            if isinstance(download_lyrics, bool) and download_lyrics:
+                download_lyrics_flag = download_lyrics
+
+            elif isinstance(download_lyrics, str) and download_lyrics.lower() == "true":
+                download_lyrics_flag = True
+
+            elif isinstance(download_lyrics, str) and download_lyrics.lower() == "false":
+                download_lyrics_flag = False
+
+            if download_lyrics_flag:
+                try:
+                    Track.get_song_lyrics(track_id, file_path.with_suffix('.lrc'))
+                    _logger.info(f"Lyrics downloaded for track {track_id}")
+
+                except ValueError:
+                    _logger.error(f"Failed to download lyrics for track {track_id}")
+
+            Track.convert_raw_to_audio(file_path)
+
+            _logger.info(
+                f"Audio converted to {Config.get('downloader', 'format', Constants.CONFIG_DOWNLOADER_FORMAT)} for track {track_id}")
+
+            try:
+                Helper.set_audio_tags(file_path, artists, name, album_name, release_year, disc_number, track_number)
+                Helper.set_music_thumbnail(file_path, image_url)
+                _logger.info(f"Audio tags set for track {track_id}")
+
+            except Exception as e:
+                _logger.error(f"Failed to set audio tags for track {track_id}: {e.with_traceback(None)}")
+
+            Database.create_downloaded_song(track_id, Downloader.account, str(file_path))
+
+        except Exception as e:
+            _logger.error(f"Failed to download track {track_id}: {e.with_traceback(None)}")
